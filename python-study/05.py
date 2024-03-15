d3a33f43f8894b8f7f5d99fb7f486ba8ebd1bfa2
권준일 <wnsdlf925@naver.com>
2024-03-15 21:21:35 +0900

    add: 05.py

---
+++ python-study/05.py
@@ -0,0 +1,20 @@
+
+def solution(arr1, arr2):
+
+    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
+
+    for i in range(len(arr1)):
+        
+        for j in range(len(arr2[0])):
+            sum = 0
+            for z in range(len(arr1[0])):
+                sum += arr1[i][z]*arr2[z][j]
+            answer[i][j] = sum
+
+
+    return answer
+
+
+
+
+print(solution([[1,4],[3,2],[4,1]],[[3,3],[3,3]])) 
