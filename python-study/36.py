2480547d44c613c0908be77a81d8054ce5fbca68
권준일 <wnsdlf925@naver.com>
2024-04-17 18:25:14 +0900

    add: 36.py

---
+++ python-study/36.py
@@ -0,0 +1,21 @@
+def solution(phone_book):
+    answer = True
+    same = list([set() for _ in range(20)])
+
+    for i in phone_book:
+        j = len(i)-1
+        for z in range(j):
+            same[z].add(i[0:z+1])
+    for i in phone_book:
+
+        j = len(i)-1
+        if i in same[j]:
+            answer = False
+        else:
+            same[j].add(i)
+        if not answer:
+            break
+    
+    return answer
+
+print(solution(["119", "97674223", "1195524421"]))
